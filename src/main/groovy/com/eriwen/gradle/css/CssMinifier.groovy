2deaf1293b8ecef94f6f9c83ee8e5d19b6a12e65
Oleksii Sapon <asapon@freeconferencecall.com>
2017-01-30 14:04:59 +0200

    added spaces to the operators for CSS calc functions

--- src/main/groovy/com/eriwen/gradle/css/CssMinifier.groovy
+++ src/main/groovy/com/eriwen/gradle/css/CssMinifier.groovy
@@ -2,6 +2,8 @@ package com.eriwen.gradle.css
 
 import com.yahoo.platform.yui.compressor.CssCompressor
 
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 /**
  * Utility class that houses minification logic.
  *
@@ -38,5 +40,41 @@ class CssMinifier {
                 writer.close()
             }
         }
+
+        fixCalcFunc(outputFile)
+    }
+
+    /**
+     * Fix issues related to calc function that came after CSS compression
+     * @param file File where function calc shall be fixed
+     */
+    private void fixCalcFunc(File file) {
+        String text = file.text
+        file.withWriter { w ->
+            w << addSpacesToOperatorsInCalcFunc(text)
+        }
+    }
+
+    /**
+     * Adds spaces to the operators for CSS calc functions
+     * @param string Input string
+     * @return Result
+     */
+    private String addSpacesToOperatorsInCalcFunc(String string) {
+        StringBuffer sb = new StringBuffer()
+        Pattern p = ~/calc\([^\)]*\)/
+        Matcher m = p.matcher(string)
+        while (m.find()) {
+            String s = m.group()
+
+            s = s.replaceAll("(?<=[%|px|em|rem|vw|\\d]+)\\+", " + ")
+            s = s.replaceAll("(?<=[%|px|em|rem|vw|\\d]+)\\-", " - ")
+            s = s.replaceAll("(?<=[%|px|em|rem|vw|\\d]+)\\*", " * ")
+            s = s.replaceAll("(?<=[%|px|em|rem|vw|\\d]+)\\/", " / ")
+
+            m.appendReplacement(sb, s)
+        }
+        m.appendTail(sb)
+        sb.toString()
     }
 }
